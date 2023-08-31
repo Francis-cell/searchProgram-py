0c3c7d7658484a902721f360fcef72d44c9a6ac6
zhumengren <franciszmr@foxmail.com>
2023-08-31 23:43:15 +0800

    excu

---
+++ main/main.py
@@ -0,0 +1,19 @@
+# coding=gbk
+'''
+项目描述：比如我想学习某方面的知识，它搜出来的结果可以附带一些例子，并举例说明
+        给出的案例贴近日常、通俗易懂
+        如果这个词，涉及实验、实验步骤等，能够提供一些视频链接，方便拓展知识
+'''
+from utils.BaiduBaikeSpiderUtils import BaiduBaikeSpiderUtils as baidus
+from flaskFiles import spiderFlask as sf
+
+if __name__ == '__main__':
+    print("关键词含义搜索项目！！！")
+    proxies = {
+        # 'http': 'http://@58.20.184.187:9091',
+        'http': 'http://@101.200.127.149:3129',
+        # 'http': 'http://@27.42.168.46:9091',
+        # 'http': 'http://@47.106.105.236:80'
+    }
+    sf.mains(proxies)
+
